90d1839d55127bcd3c2a02733690f2d9abfa1a81
Karthik Dileep <karthikdileep003@gmail.com>
2022-02-21 20:45:27 +0530

    UI fixed

--- lib/main.dart
+++ lib/main.dart
@@ -17,88 +17,92 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Additional Information'),
-            centerTitle: true,
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {},
-            ),
+        appBar: AppBar(
+          title: Text('Additional Information'),
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {},
           ),
-          body: ListView(
-            children: [
-              CustomListTile(
-                title: 'Share Dukaan App',
-                leading: IconButton(
-                  icon: Icon(Icons.share_outlined),
-                  onPressed: () {},
-                ),
-                trailing: IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.arrow_forward_ios_outlined),
-                ),
+        ),
+        body: ListView(
+          children: [
+            CustomListTile(
+              title: 'Share Dukaan App',
+              leading: IconButton(
+                icon: Icon(Icons.share_outlined),
+                onPressed: () {},
               ),
-              CustomListTile(
-                title: 'Change Language',
-                leading: IconButton(
-                  icon: Icon(Icons.messenger_outline_outlined),
-                  onPressed: () {},
-                ),
-                trailing: IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.arrow_forward_ios_outlined),
-                ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.arrow_forward_ios_outlined),
               ),
-              CustomListTile(
-                  title: 'WhatsApp Chat Support',
-                  leading: IconButton(
-                    icon: FaIcon(FontAwesomeIcons.whatsapp),
-                    onPressed: () {},
+            ),
+            CustomListTile(
+              title: 'Change Language',
+              leading: IconButton(
+                icon: Icon(Icons.messenger_outline_outlined),
+                onPressed: () {},
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.arrow_forward_ios_outlined),
+              ),
+            ),
+            CustomListTile(
+                title: 'WhatsApp Chat Support',
+                leading: IconButton(
+                  icon: FaIcon(FontAwesomeIcons.whatsapp),
+                  onPressed: () {},
+                ),
+                trailing: Switch(
+                  value: radioButton,
+                  activeTrackColor: Colors.blue[100],
+                  activeColor: Colors.blue[900],
+                  onChanged: (value) {},
+                )),
+            CustomListTile(
+              title: 'Privacy Policy',
+              leading: IconButton(
+                icon: Icon(Icons.lock_outline),
+                onPressed: () {},
+              ),
+            ),
+            CustomListTile(
+              title: 'Rate Us',
+              leading: IconButton(
+                icon: Icon(Icons.star_border),
+                onPressed: () {},
+              ),
+            ),
+            CustomListTile(
+              title: 'Sign Out',
+              leading: IconButton(
+                icon: Icon(Icons.logout_outlined),
+                onPressed: () {},
+              ),
+            ),
+            SizedBox(
+              height: 300,
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Version',
+                    style: TextStyle(fontSize: 18),
                   ),
-                  trailing: Switch(
-                    value: radioButton,
-                    activeTrackColor: Colors.blue[100],
-                    activeColor: Colors.blue[900],
-                    onChanged: (value) {},
-                  )),
-              CustomListTile(
-                title: 'Privacy Policy',
-                leading: IconButton(
-                  icon: Icon(Icons.lock_outline),
-                  onPressed: () {},
-                ),
+                  Text(
+                    '2.4.2',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                  )
+                ],
               ),
-              CustomListTile(
-                title: 'Rate Us',
-                leading: IconButton(
-                  icon: Icon(Icons.star_border),
-                  onPressed: () {},
-                ),
-              ),
-              CustomListTile(
-                title: 'Sign Out',
-                leading: IconButton(
-                  icon: Icon(Icons.logout_outlined),
-                  onPressed: () {},
-                ),
-              ),
-              SizedBox(
-                height: 290,
-              ),
-              Center(
-                child: Text(
-                  'Version',
-                  style: TextStyle(fontSize: 17),
-                ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Center(
-                child: Text('2.4.2'),
-              )
-            ],
-          )),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
